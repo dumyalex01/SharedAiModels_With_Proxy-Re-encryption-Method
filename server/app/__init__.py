352a520c096e7a092f67ba7a9bfc4aafb2988255
Alexandru Duminica <alexandru.duminica@stsnet.ro>
2026-01-21 15:23:50 +0200

    endpoint validat

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,7 @@ from app.extensions import db
 from app.routes.auth import bp as auth_bp
 from app.routes.request import bp as request_bp
 from app.routes.attachment import bp as attachement_bp
+from app.routes.keys import bp as keys_bp
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +14,6 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix="/v1/api/auth")
     app.register_blueprint(request_bp, url_prefix="/v1/api/request")
     app.register_blueprint(attachement_bp,url_prefix = "/v1/api/attachment")
+    app.register_blueprint(keys_bp,url_prefix="/v1/api/keys")
 
     return app
